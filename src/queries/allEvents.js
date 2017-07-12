e6e6172ff29c4d866a1f5ff05647f2feb726b7ac
Jamie Barton <hi_jamie@me.com>
2017-07-12 08:01:39 +0100

    Added basic pagination

--- src/queries/allEvents.js
+++ src/queries/allEvents.js
@@ -1,15 +1,32 @@
-const { GraphQLList } = require('graphql')
+const { GraphQLList, GraphQLInt } = require('graphql')
 const EventType = require('../types/event')
 const getProjection = require('../utils/projection')
 
 module.exports = {
   type: new GraphQLList(EventType),
-  resolve: (root, args, { db: { Event } }, fieldASTs) => {
+  args: {
+    first: {
+      name: 'first',
+      type: GraphQLInt
+    },
+    skip: {
+      name: 'skip',
+      type: GraphQLInt
+    }
+  },
+  resolve: (
+    root,
+    { first = null, skip = null },
+    { db: { Event } },
+    fieldASTs
+  ) => {
     return new Promise((resolve, reject) => {
       const projection = getProjection(fieldASTs)
 
       Event.find({})
         .select(projection)
+        .skip(skip)
+        .limit(first)
         .exec()
         .then(data => resolve(data))
         .catch(errors => reject(errors))
