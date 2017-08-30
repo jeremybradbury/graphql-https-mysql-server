24700dd6336f52334a9276c94cbc2b8495709502
Jeremy Bradbury <jdbradbury@gmail.com>
2017-08-29 17:02:00 -0700

    fix this table ref

--- src/db/Sessions.js
+++ src/db/Sessions.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 
 const Sessions = function(sequelize, DataTypes){ 
   /* schema */
-  let Session = sequelize.define('Sessions', {
+  let Session = sequelize.define('Session', {
     'session_id': {
 			type: DataTypes.STRING(32),
 			primaryKey: true,
